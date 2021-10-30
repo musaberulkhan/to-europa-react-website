547702f71a569282e83837b83c038ddb59a1b9df
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-10-31 02:41:36 +0600

    Discount Section Added

---
+++ src/Components/Discount/Discount.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Discount.css';
+import Sale from '../../Images/sale.jpg';
+import { Link } from 'react-router-dom';
+
+const Discount = () => {
+    return (
+        <div className="discount">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-12">
+                        <img className="img-fluid p-5" src={Sale} alt="" />
+                    </div>
+                    <div className="col-md-6 col-12 p-5 d-flex align-items-center">
+                        <div>
+                            <h1 className="title"> Biggest <span>11.11 </span> Shopping day is on the way!</h1>
+                            <h4>Follow our website and get attractive discount upto 50%!</h4>
+                            <Link className="btn btn-dark px-4 mt-3" to="/packages">View All Packages</Link>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Discount;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Discount from '../Discount/Discount';
 import Offers from '../Offers/Offers';
 import Packages from '../Packages/Packages';
 
 const Home = () => {
     return (
         <div>
-            <Banner></Banner>
-            <Offers></Offers>
+            <Banner></Banner>            
             <Packages></Packages>
+            <Discount></Discount>
+            <Offers></Offers>
         </div>
     );
 };
