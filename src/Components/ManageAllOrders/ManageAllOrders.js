fa577164992250dbe3b69559e37d4a24d08a766d
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-10-31 18:17:53 +0600

    Manage All Orders Designed

--- src/Components/ManageAllOrders/ManageAllOrders.js
+++ src/Components/ManageAllOrders/ManageAllOrders.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './ManageAllOrders.css';
+import ManageOrder from './ManageOrder/ManageOrder';
 
 const ManageAllOrders = () => {
+    const [orders, setOrders] = useState([]);
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/orders`)
+            .then(res => res.json())
+            .then(data => setOrders(data));
+    }, []);
+
+    console.log(orders);
+
+    const handleCancelBookingButton = id => {
+        const processed = window.confirm('Are you sure you want to delete?');
+        if (processed) {
+            fetch(`http://localhost:5000/orders/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {                        
+                        const remainingOrders = orders.filter(order => order._id !== id);
+                        setOrders(remainingOrders);
+                        alert('Deleted');
+                    }
+                });
+        }
+    }
+
     return (
-        <div className="manage-orders py-5">
+        <div className="manage-all-orders py-5 container">
             <h1 className="text-center">Manage All Orders</h1>
+            {
+                orders?.map(order => <ManageOrder
+                    key={order._id}
+                    order={order}
+                    handleCancelBookingButton={handleCancelBookingButton}
+                >
+                </ManageOrder>)
+            }
         </div>
     );
 };
---
+++ src/Components/ManageAllOrders/ManageOrder/ManageOrder.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import './ManageOrder.css';
+
+const ManageOrder = (props) => {
+    const { _id, name, address, thana, district, packageId, payment, status } = props.order;
+    const [_package, setPackage] = useState({});
+
+    useEffect(() => {
+        fetch(`http://localhost:5000/packages/${packageId}`)
+            .then(res => res.json())
+            .then(data => setPackage(data));
+    }, []);
+
+    return (
+        <div className="manage-order px-4 py-3 my-4">
+            <div className="row">
+                <div className="col-md-6 col-12 my-2 d-flex align-items-center">
+                    <div>
+                        <img className="img-fluid package-image me-2" src={_package?.image} alt="" />
+                    </div>
+                    <div>
+                        <h4>{_package?.name}</h4>
+                        <h6>Duration: {_package?.duration}</h6>
+                        <h6>Price: {_package?.price}</h6>
+                        <h6>Status: {status}</h6>
+                    </div>
+                </div>
+                <div className="col-md-6 col-12 my-2">
+                    <h5>Billing Information</h5>
+                    <p className="mb-0">Name: {name}</p>
+                    <p className="mb-0">Address: {`${address}, ${thana}, ${district}.`}</p>
+                    <p className="mb-0">Payment: {payment}</p>
+                    <button className="btn btn-danger">Cancel Booking</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ManageOrder;
